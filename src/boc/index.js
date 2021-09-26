534d6b137055c131465a3b3d4503e1c5edf76a18
veryMaker <85267430+veryMaker@users.noreply.github.com>
2021-09-26 10:58:36 +0300

    Merge remote-tracking branch 'upstream/dev'

--- src/boc/index.js
+++ src/boc/index.js
@@ -1,4 +1,5 @@
 const {BitString} = require("./BitString");
 const {Cell} = require("./Cell");
+const {parseDict} = require("./Dict");
 
-module.exports = {BitString, Cell};
+module.exports = {BitString, Cell, parseDict};
