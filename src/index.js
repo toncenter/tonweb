d06044f9cc47e68a190919e766d5a103cee9e5c9
tolya-yanot <tolya-yanot>
2022-01-20 22:55:27 +0300

    0.0.27

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const {SubscriptionContract} = require("./contract/subscription/index");
 const TransportWebUSB = require("@ledgerhq/hw-transport-webusb").default;
 const TransportWebHID = require("@ledgerhq/hw-transport-webhid").default;
 const BluetoothTransport = require("@ledgerhq/hw-transport-web-ble").default;
-const version = '0.0.26e';
+const version = '0.0.27';
 
 class TonWeb {
     constructor(provider) {
