e132173295d2ee2283df0d18871a6f25bc948cf2
tolya-yanot <1449561+tolya-yanot@users.noreply.github.com>
2022-03-31 01:33:17 +0300

    update jetton contracts

--- src/contract/token/ft/JettonMinter.js
+++ src/contract/token/ft/JettonMinter.js
@@ -1,6 +1,7 @@
 const {Contract} = require("../../index");
 const {Cell} = require("../../../boc");
 const {createOffchainUriCell, parseOffchainUriCell, parseAddress} = require("../nft/NftUtils");
+const {BN} = require("../../../utils");
 
 /**
  * ATTENTION: this is DRAFT, there will be changes
@@ -13,7 +14,7 @@ class JettonMinter extends Contract {
      */
     constructor(provider, options) {
         options.wc = 0;
-        options.code = options.code || Cell.oneFromBoc('B5EE9C7241020701000133000114FF00F4A413F4BCF2C80B0102016202030202CD0405001FA13C5BDA89A1F401F481A9A860FEAA4101C1D1910E38048ADF068698180B8D848ADF07D201800E98FE99FF6A2687D007D206A6A18400AA9385D47149B28B0E382F97024817D007D207D00182A0900AA2383F803AC502099E428027D012C678B666664F6AA701AC0090B5D71812F834207F97840600A5F7C142281B82A1009AA0A01E428027D012C678B00E78B666491646580897A007A00658064907C80383A6465816503E5FFE4E840073B400C646582A801E78B28037D0165B5E609E58F89659F80FD0164B87D804009E02FA00FA4030F8282570542013541403C85004FA0258CF1601CF16CCC922C8CB0112F400F400CB00C9F9007074C8CB02CA07CBFFC9D05006C705F2E2C35004A15520C85004FA0258CF16CCCCC9ED549C3AF745');
+        options.code = options.code || Cell.oneFromBoc('B5EE9C72410209010001AA000114FF00F4A413F4BCF2C80B0102016202030202CC040502037A60070801D5D9910E38048ADF068698180B8D848ADF07D201800E98FE99FF6A2687D007D206A6A18400AA9385D47181A9AA8AAE382F9702480FD207D006A18106840306B90FD001812881A28217804502A906428027D012C678B666664F6AA7041083DEECBEF0BDD71812F83C207F9784060093DFC142201B82A1009AA0A01E428027D012C678B00E78B666491646580897A007A00658064907C80383A6465816503E5FFE4E83BC00C646582AC678B28027D0109E5B589666664B8FD80400FC03FA00FA40F82854120870542013541403C85004FA0258CF1601CF16CCC922C8CB0112F400F400CB00C9F9007074C8CB02CA07CBFFC9D05008C705F2E04A12A1035024C85004FA0258CF16CCCCC9ED5401FA403020D70B01C3008E1F8210D53276DB708010C8CB055003CF1622FA0212CB6ACB1FCB3FC98042FB00915BE2007DADBCF6A2687D007D206A6A183618FC1400B82A1009AA0A01E428027D012C678B00E78B666491646580897A007A00658064FC80383A6465816503E5FFE4E840001FAF16F6A2687D007D206A6A183FAA9040F6B06B3C');
         super(provider, options);
     }
 
@@ -32,16 +33,26 @@ class JettonMinter extends Contract {
     }
 
     /**
-     * params   {{tokenAmount: BN, destination: Address, amount: BN, queryId?: number}}
+     * params   {{jettonAmount: BN, destination: Address, amount: BN, queryId?: number}}
      * @return {Cell}
      */
-    createMintBody(params) {
+     createMintBody(params) {
         const body = new Cell();
         body.bits.writeUint(21, 32); // OP mint
         body.bits.writeUint(params.queryId || 0, 64); // query_id
-        body.bits.writeCoins(params.tokenAmount);
         body.bits.writeAddress(params.destination);
-        body.bits.writeCoins(params.amount);
+        body.bits.writeCoins(params.amount); // in Toncoins
+
+        const transferBody = new Cell(); // internal transfer
+        transferBody.bits.writeUint(0x178d4519, 32); // internal_transfer op
+        transferBody.bits.writeUint(params.queryId || 0, 64);
+        transferBody.bits.writeCoins(params.jettonAmount);
+        transferBody.bits.writeAddress(null); // from_address
+        transferBody.bits.writeAddress(null); // response_address
+        transferBody.bits.writeCoins(new BN(0)); // forward_amount
+        transferBody.bits.writeBit(false); // forward_payload in this slice, not separate cell
+
+        body.refs[0] = transferBody;
         return body;
     }
 
